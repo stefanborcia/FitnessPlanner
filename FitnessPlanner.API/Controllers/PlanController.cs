65a7e9db8011fc8e03e92545fbde396425205217
stefanborcia <stefigabi89@yahoo.com>
2025-05-13 11:48:44 +0200

    Create PlanController

---
+++ FitnessPlanner.API/Controllers/PlanController.cs
@@ -0,0 +1,32 @@
+﻿using FitnessPlanner.API.DTOs;
+using FitnessPlanner.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessPlanner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanController : ControllerBase
+    {
+        private readonly IWorkoutPlanService _workoutService;
+
+        public PlanController(IWorkoutPlanService workoutService)
+        {
+            _workoutService = workoutService;
+        }
+
+        [HttpPost("workout")]
+        public IActionResult GenerateWorkoutPlan([FromBody] PlanRequestDto request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Request object is null");
+            }
+
+            var plan = _workoutService.GenerateWorkoutPlan(request.Goal, request.BodyType);
+            return Ok(plan);
+        }
+
+    }
+}
---
+++ FitnessPlanner.API/DTOs/PlanRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using FitnessPlanner.Domain.Enums;
+
+namespace FitnessPlanner.API.DTOs
+{
+    public class PlanRequestDto
+    {
+        public FitnessGoal Goal { get; set; }
+        public BodyType BodyType { get; set; }
+    }
+}
