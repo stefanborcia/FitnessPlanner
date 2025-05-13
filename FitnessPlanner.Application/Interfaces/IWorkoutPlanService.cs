d4d79d96b180769ff4e163d2074104cd86d68ef8
stefanborcia <stefigabi89@yahoo.com>
2025-05-13 10:46:59 +0200

    Create Interfaces

---
+++ FitnessPlanner.Application/Interfaces/IWorkoutPlanService.cs
@@ -0,0 +1,15 @@
+﻿using FitnessPlanner.Application.DTOs;
+using FitnessPlanner.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPlanner.Application.Interfaces
+{
+    public interface IWorkoutPlanService
+    {
+        WorkoutPlanDto GenerateWorkoutPlan(FitnessGoal goal, BodyType bodyType);
+    }
+}
