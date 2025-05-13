67dc7b3a294bd960c199f16c2b65ad7aab5844d2
stefanborcia <stefigabi89@yahoo.com>
2025-05-13 10:54:09 +0200

    Create Application Dependency Injection Setup

---
+++ FitnessPlanner.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPlanner.Application.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Register services here when implemented
+            // services.AddScoped<IWorkoutPlanService, WorkoutPlanService>();
+
+            return services;
+        }
+    }
+}
