5d9f1ae0e1037b6a0a34e1f1c353bf7486ec731c
stefanborcia <stefigabi89@yahoo.com>
2025-05-13 10:50:36 +0200

    Create Mapping Profiles with AutoMapper

---
+++ FitnessPlanner.Application/Mapping/DomainToDtoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FitnessPlanner.Application.DTOs;
+using FitnessPlanner.Domain.Entities;
+
+namespace FitnessPlanner.Application.Mapping
+{
+    public class DomainToDtoProfile : Profile
+    {
+        public DomainToDtoProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Goal, opt => opt.MapFrom(src => src.Goal.ToString()))
+                .ForMember(dest => dest.BodyType, opt => opt.MapFrom(src => src.BodyType.ToString()));
+
+            CreateMap<Exercise, ExerciseDto>();
+            CreateMap<WorkoutPlan, WorkoutPlanDto>();
+        }
+    }
+}
